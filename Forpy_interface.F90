d6f06166ce3c8ed6b444ac3705f3ad50916e9265
chzhangudel <chzhang@udel.edu>
2022-11-30 13:46:00 -0500

    Arbitary  number of nput and output variables for Python script

--- Forpy_interface.F90
+++ Forpy_interface.F90
@@ -38,54 +38,47 @@ subroutine forpy_run_python_init(CS,python_dir,python_file)
 end subroutine forpy_run_python_init
 
 !> !> Send variables to a python script and output the results
-subroutine forpy_run_python(in1, in2, out1, out2, CS, TopLayer)
+subroutine forpy_run_python(in1, out1, CS, TopLayer)
     type(python_interface),        intent(in)  :: CS     !< Python interface object
   ! Local Variables
     logical, intent(in) :: TopLayer             !< If true, only top layer is used.
-    real, dimension(:,:,:), &
-                                    intent(in) :: in1     ! First input variable.
-    real, dimension(:,:,:), &
-                                    intent(in) :: in2     ! second input variable.
-    real, dimension(:,:,:), &
-                                    intent(inout) :: out1      ! First output variable.
-    real, dimension(:,:,:), &
-                                    intent(inout) :: out2      ! second output variable.
+    real, dimension(:,:,:,:), &
+                                    intent(in) :: in1     ! input variables.
+    real, dimension(:,:,:,:), &
+                                    intent(inout) :: out1      ! output variables.
   ! Local Variables for Forpy
     integer :: ierror ! return code from python interfaces
-    type(ndarray) :: in1_py,in2_py,out_arr      !< first and second variables in the form of numpy array
+    type(ndarray) :: in1_py,out_arr         !< variables in the form of numpy array
     type(object)  :: obj                    !< return objects
     type(tuple)   :: args                   !< input arguments for the Python module
     real, dimension(:,:,:,:), pointer :: out_for  !< outputs from Python module
     integer :: current_pe
     integer :: hi, hj ! temporary
-    integer :: i, j, k
-    integer :: nztemp
+    integer :: i, j, k, l
+    integer :: nztemp, out_num
 
     current_pe = PE_here()
 
     if (TopLayer) then
       nztemp = 1
     else
-      nztemp = size(in1,3)
+      nztemp = size(in1,4)
     endif
 
   ! Covert input into Forpy Numpy Arrays 
     if (TopLayer) then
-        ierror = ndarray_create(in1_py, in1(:,:,1))
-        ierror = ndarray_create(in2_py, in2(:,:,1))
+        ierror = ndarray_create(in1_py, in1(:,:,:,1))
       else
         ierror = ndarray_create(in1_py, in1)
-        ierror = ndarray_create(in2_py, in2)
       endif
       if (ierror/=0) then; call err_print; endif
     
       ! Create Python Argument 
-      ierror = tuple_create(args,4)
+      ierror = tuple_create(args,3)
       if (ierror/=0) then; call err_print; endif
       ierror = args%setitem(0,in1_py)
-      ierror = args%setitem(1,in2_py)
-      ierror = args%setitem(2,current_pe)
-      ierror = args%setitem(3,num_PEs())
+      ierror = args%setitem(1,current_pe)
+      ierror = args%setitem(2,num_PEs())
       if (ierror/=0) then; call err_print; endif
     
       ! Invoke Python 
@@ -98,24 +91,22 @@ subroutine forpy_run_python(in1, in2, out1, out2, CS, TopLayer)
     
       ! Destroy Objects
       call in1_py%destroy
-      call in2_py%destroy
       call out_arr%destroy
       call obj%destroy
       call args%destroy
 
       ! find the margin size (if order='C')
-      hi = (size(out1,1) - size(out_for,3))/2
-      hj = (size(out1,2) - size(out_for,2))/2
+      hi = (size(out1,2) - size(out_for,3))/2
+      hj = (size(out1,3) - size(out_for,2))/2
     
       ! Output (out_for in C order has index (nk,nj,ni))
                       ! in F order has index (ni,nj,nk)
-      out1 = 0.0; out2 = 0.0;
+      out1 = 0.0
       do k=1,nztemp
-        do j=1,size(out_for,2) ; do i=1,size(out_for,3)
-          out1(i+hi,j+hj,k) = out_for(k,j,i,1) ! if order='C'
-          ! out1(i+hi,j+hj,k) = out_for(1,i,j,k) ! if order='F'
-          out2(i+hi,j+hj,k) = out_for(k,j,i,2)
-        enddo ; enddo
+        do j=1,size(out_for,2) ; do i=1,size(out_for,3); do l=1,size(out_for,4)
+          out1(l,i+hi,j+hj,k) = out_for(k,j,i,l) ! if order='C'
+          ! out1(l,i+hi,j+hj,k) = out_for(l,i,j,k) ! if order='F'
+        enddo ; enddo ; enddo
       enddo
   
 end subroutine forpy_run_python 
